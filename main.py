8d507ecb6afeadcdbb7a82e98e10c8416021dd7e
Fiamma <stavtxxx1@gmail.com>
2026-01-02 10:54:24 +0200

    add transforming functions

--- main.py
+++ main.py
@@ -13,9 +13,25 @@ def swedish_pump(b):
     return f_b
 
 
+def swedish_pump_vector_to_binary_string(b):
+    n = len(b)
+    binary_string = np.zeros(n)
+    for i in range(n):
+        if b[i] == 1:
+            binary_string[i] = 1
+    return binary_string
 
-
+def binary_string_vector_to_swedish_pump_vector(binary_string):
+    n = len(binary_string)
+    swedish_pump_vector = np.ones(n)
+    for i in range(n):
+        if binary_string[i] == 0:
+            swedish_pump_vector[i] = -1
+    return swedish_pump_vector
 
 if __name__ == "__main__":
-    swedishtry = swedish_pump(np.array([1, 1, -1, 1, 1]))
-    print(swedishtry)
+    vec = np.array([1, 1, -1, -1, 1])
+    swedish_to_bin = swedish_pump_vector_to_binary_string(vec)
+    bin_to_swedish = binary_string_vector_to_swedish_pump_vector(swedish_to_bin)
+    print(swedish_to_bin)
+    print(bin_to_swedish)
